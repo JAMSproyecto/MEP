31f4d77a3d67b416589b4371fc9d4f46cdaec1d7
jjimenezg19 <47197113+jjimenezg19@users.noreply.github.com>
2019-03-19 17:31:20 -0600

    commit estilos
    se agregaron los estilos a el caso de uso Articulos, ya estan terminados los tres casos de uso.

--- api/componentes/lista_utiles/lista_utiles.model.js
+++ api/componentes/lista_utiles/lista_utiles.model.js
@@ -3,7 +3,8 @@
 const mongoose = require('mongoose');
 
 let schema_utiles = new mongoose.Schema(
-    {   codigo :{type : String, require : true},
+    {   //codigo :{type : mongoose.Types.ObjectId, ref: 'centro_educativo_', require : true},
+        codigo : {type : String, required : true},
         tipo: {type :String, required : true},
         nombre : {type : String, required : true},
         anno : {type : String, required : false},
--- public/js/controlador_listar_articulos.js
+++ public/js/controlador_listar_articulos.js
@@ -1,17 +1,27 @@
 'use strict';
 
 const tabla = document.querySelector('#tbl_articulos tbody');
+const input_filtrar = document.querySelector('#txt_filtrar');
 
-let mostrar_datos = () =>{
-    let articulos = listar_articulos();
+let articulos = listar_articulos();
 
+let mostrar_datos = () =>{    
+    let filtro = input_filtrar.value;
+    tabla.innerHTML = '';
     for (let i = 0; i < articulos.length; i++) {
+      if (articulos[i]['codigo'].toLowerCase().includes(filtro.toLowerCase()) || 
+         articulos[i]['nombre'].toLowerCase().includes(filtro.toLowerCase()) ||
+         articulos[i]['descripcion'].toLowerCase().includes(filtro.toLowerCase())) {
+
         let fila = tabla.insertRow();
         fila.insertCell().innerHTML = articulos[i]['codigo'];
         fila.insertCell().innerHTML = articulos[i]['nombre'];
-        fila.insertCell().innerHTML = articulos[i]['descripcion']; 
+        fila.insertCell().innerHTML = articulos[i]['descripcion'];
+      }  
     }
 };
+input_filtrar.addEventListener('keyup', mostrar_datos);
 mostrar_datos();
+
 
 
