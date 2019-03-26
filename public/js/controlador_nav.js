586fb56fed7c4f2043395cf1ee5dd2aa0c110fb7
Jei <jhidalgoo@ucenfotec.ac.cr>
2019-03-26 12:13:05 -0600

    Se corrige validación

--- public/js/controlador_nav.js
+++ public/js/controlador_nav.js
@@ -7,27 +7,31 @@ let conectado = sessionStorage.getItem('conectado');
 let tipoUsuario = sessionStorage.getItem('tipoUsuario');
 
 
-if(null !== conectado){
-    switch(tipoUsuario){
-        case 'administrador':
-
-        break;
-        case 'PadreFamilia':
-            enlaces[0].classList.add('ocultar');
-            enlaces[1].classList.add('ocultar');
-            enlaces[2].classList.add('ocultar');
-        break;
-
-        default: 
-
-        break;
-    }
+if(null !== conectado && conectado === true){
+    if(null !== tipoUsuario){
+        switch(tipoUsuario){
+            case 'administrador':
+        
+            break;
+            case 'PadreFamilia':
+                enlaces[0].classList.add('ocultar');
+                enlaces[1].classList.add('ocultar');
+                enlaces[2].classList.add('ocultar');
+            break;
+        
+            default: 
+        
+            break;
+        }
+    }else{
+		console.error('No se encontró el tipo de usuario');
+	}
 
 }else{
     window.location.href = 'inicio_sesion.html';
 }
 
-function cerrar_sesion(){
+let cerrar_sesion = () => {
     sessionStorage.clear();
     window.location.href = 'inicio_sesion.html';
 };
