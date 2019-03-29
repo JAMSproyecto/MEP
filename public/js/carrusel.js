e9e0dbdef920c074bdbbaec8883648d5ce20a024
Angie <ahernandezb@ucenfotec.ac.cr>
2019-03-28 22:36:15 -0600

    listo perfil admin
    Listo bichos

---
+++ public/js/carrusel.js
@@ -0,0 +1,18 @@
+$('.your-class').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    
+  });
+
+
+
+  $('.noticias').slick({
+    centerPadding: '60px',
+ 
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 9000,
+  });
