48e532e095d26ab69b8729fecf875ee4c7e1f878
Andres <acalderonc@ucenfotec.ac.cr>
2019-03-30 23:29:17 -0600

    Perfil Estilos

--- public/js/controlador_perfil_cedu.js
+++ public/js/controlador_perfil_cedu.js
@@ -2,6 +2,19 @@
 
 let crearPerfil = (perfil)=>{
     document.querySelector('#titulo_centro_educativo').innerHTML = perfil.Nombre;
+
+    let actividad = document.createElement('div');
+    actividad.classList.add('actividad');
+    
+    let strong = document.createElement('strong');
+    strong.classList.add('nombre__actividad');
+
+    let fecha = document.createElement('p');
+    fecha.classList.add('fecha__actividad');
+
+    let hora = document.createElement('p');
+    fecha.classList.add('hora__actividad');
+
 };
 
 window.addEventListener('load', () => {
